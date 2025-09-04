e40094826587a546f7b4140459e7aa904dd23d84
ImrenR <imrenrahbay@gmail.com>
2025-09-04 22:37:55 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,51 @@
-import React, {useState} from "react";
-import PatientList from "../components/PatientList"
+import React, { useState } from "react";
+import PatientList from "../components/PatientList";
 import AddPatient from "../components/AddPatient";
 import { hastaData, doctorData } from "../helper/Data";
 
 const Home = () => {
   const [doctors, setDoctors] = useState(doctorData);
   const [patients, setPatients] = useState(hastaData);
+  const [magic, setMagic] = useState(true);
 
-const [magic, setMagic] = useState(true)
+  const doctorClick = (dId) => {
+    setDoctors(doctors.filter((b) => b.id == dId));
+    setMagic(false);
+  };
 
-const doctorClick=(dId)=>{
- setDoctors(doctors.filter((b)=>b.id == dId))
-}
   return (
-    <div style={{ display: "block" }}>
+    <div style={{ display: magic ? "block" : "flex" }}>
       <div>
         <header>
           <h1> Porto Care Hospital</h1>
           <div className="dr">
             {doctors.map((dr) => (
               <div className="doctorCard">
-                <img 
-                src={dr.doctorImg} 
-                alt="" 
-                width="160px"
-                height="220px"
-                className="doctorBtn"
-                onClick={()=>doctorClick(dr.id)}
+                <img
+                  src={dr.doctorImg}
+                  alt=""
+                  width="160px"
+                  height="220px"
+                  className="doctorBtn"
+                  onClick={() => doctorClick(dr.id)}
+                  style={{ background: magic ? "" : "lightblue" }}
                 />
 
-                <h4 className="nameDr">{dr.doctorName}</h4>
+                <h4
+                  style={{ background: magic ? "" : "lightblue", borderLeft: `10px solid ${magic ? "" : "darkblue"}` }}
+                  className="nameDr"
+                >
+                  {dr.doctorName}
+                </h4>
               </div>
             ))}
           </div>
         </header>
 
-        <AddPatient />
+        {/* <AddPatient /> */}
       </div>
 
-      <PatientList patients={patients} setPatients={setPatients}/>
+      <PatientList patients={patients} setPatients={setPatients} />
     </div>
   );
 };
